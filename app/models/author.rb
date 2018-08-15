2cfedc66a07aeaca809451120fae05678283d510
Jason Pitts <juniorjr083@gmail.com>
2018-08-15 20:43:09 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ActiveRecord::Base
-  validates :name , presence: true, length: { minimun: 10 }
+  validates :name , presence: true
    validates :phone_number , presence: true, length: { minimun: 10 }
 end

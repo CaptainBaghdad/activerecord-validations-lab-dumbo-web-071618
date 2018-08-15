9ebba0fd62c95d6c7893ef7db58a78c699f2c546
Jason Pitts <juniorjr083@gmail.com>
2018-08-15 21:42:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :clickbait
 
-  ans = self.title
+  ans = title
   def clickbait
     words = [ "Won\'t Believe","Secret", "Top","Guess"]
     puts "This is the ans #{ans}"

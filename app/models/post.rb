09731407408a12233115d1e94f3693f2f77c2ce5
Jason Pitts <juniorjr083@gmail.com>
2018-08-15 21:35:32 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,14 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  #validate :clickbait
+  validate :clickbait
 
 
- 
+  def clickbait
+    words = [ "Won\'t Believe","Secret", "Top","Guess"]
+    
+   words.any? {|w| title.include?(w)}
+
+end
 
 end
